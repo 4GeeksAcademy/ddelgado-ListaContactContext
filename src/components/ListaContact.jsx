27300b26f5f31df9da7c4b3608e3cc764d027d56
DaysiMassielka <54219496+DaysiMassielka@users.noreply.github.com>
2025-07-29 20:28:34 +0000

    Lista de Contactos VM

--- src/components/ListaContact.jsx
+++ src/components/ListaContact.jsx
@@ -1,25 +1,51 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ContactoCard from "./ContactoCard";
+import ContactoForm from "./ContactoForm"
 
 
 const App = () => {
   const [contactos, setContactos] = useState([]); // tu lista
   const [contactoEditando, setContactoEditando] = useState(null);
-
-  const eliminarContacto = (id) => {
-    setContactos(contactos.filter((c) => c.id !== id));
-  };
-
+  // Funcion para obtener los contactos de la API (GET)
+  const obtenerContactos = async () =>{
+    try {
+      const response = await fetch("https://playground.4geeks.com/contact/agendas/ddelgado/contacts")
+      const data = await response.json()
+      setContactos(data.contacts || [])
+    } catch (error) {
+      console.error("Error al ahcer el Get:", error)
+    }
+  }
+  useEffect(()=>{
+    obtenerContactos()
+  },[])
+  const eliminarContacto = async (id) =>{
+    try {
+      const response = await fetch(`https://playground.4geeks.com/contact/agendas/ddelgado/contacts/${id}`,{
+     method:"DELETE",
+      })
+      setContactos( contactos.filter((c) => c.id !== id));
+    } catch (error) {
+      console.error("Error al eliminar contacto:", error)
+    }
+  }
   const editarContacto = (id) => {
     const contacto = contactos.find((c) => c.id === id);
     setContactoEditando(contacto);
   };
-
-  const guardarContacto = (contactoActualizado) => {
-    setContactos(contactos.map((c) => (c.id === contactoActualizado.id ? contactoActualizado : c)));
-    setContactoEditando(null); 
-  };
-
+  const guardarContacto = async (contactoActualizado)=>{
+    try {
+      const response = await fetch(`https://playground.4geeks.com/contact/agendas/ddelgado/contacts/${contactoActualizado.id}`,{
+        method:"PUT",
+        headers:{"Content-Type" : "application/json"},
+        body: JSON.stringify(contactoActualizado)
+      })
+      setContactos(contactos.map((c)=>(c.id ===contactoActualizado.id ? contactoActualizado : c)))
+      setContactoEditando(null)
+    } catch (error) {
+      console.error("Error en el Put:", error)
+    }
+  }
   return (
     <div className="container mt-4">
       {contactoEditando ? (
